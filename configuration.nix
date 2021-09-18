dc2ed939f6cffb235f54244dd61c61c6b847d8f9
Vasily-Ermakov <rec0rd.alterego@gmail.com>
2021-09-18 16:41:49 +0000

    #3 xmonad installation

--- configuration.nix
+++ configuration.nix
@@ -4,6 +4,8 @@
   boot.loader.grub.device = "/dev/sda";
   networking.useDHCP = false; # deprecated
   networking.interfaces.enp0s3.useDHCP = true; # systemd Ethernet bus number 3
+  services.xserver.enable = true;
+  services.xserver.windowManager.xmonad.enable = true;
   environment.systemPackages = with pkgs; [
     git
   ];
