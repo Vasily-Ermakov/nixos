bb55df483caed45ad9db2c5cefbb21dfb224b018
Vasily-Ermakov <rec0rd.alterego@gmail.com>
2023-07-13 06:31:06 +0000

    #7 version update; EFI system partition

--- configuration.nix
+++ configuration.nix
@@ -1,13 +1,12 @@
 { config, pkgs, ... }:
 {
   imports = [ ./hardware-configuration.nix ];
-  boot.loader.grub.device = "/dev/sda";
-  networking.useDHCP = false; # deprecated
-  networking.interfaces.enp0s3.useDHCP = true; # systemd Ethernet bus number 3
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
   services.xserver.enable = true;
   services.xserver.windowManager.xmonad.enable = true;
   environment.systemPackages = with pkgs; [
     git
   ];
-  system.stateVersion = "21.05";
+  system.stateVersion = "23.05";
 }
