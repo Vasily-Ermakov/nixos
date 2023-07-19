5aecd4880b7fcf96c6cb0cfc54b692bf5728efd2
Vasily-Ermakov <rec0rd.alterego@gmail.com>
2023-07-19 06:46:04 +0000

    Firefox installation

--- configuration.nix
+++ configuration.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, pkgs,  ... }:
 {
   imports = [ ./hardware-configuration.nix ];
   boot.loader.systemd-boot.enable = true;
@@ -7,6 +7,15 @@
   services.xserver.windowManager.xmonad.enable = true;
   environment.systemPackages = with pkgs; [
     git
+    (wrapFirefox firefox-esr-unwrapped {
+      nixExtensions = [
+        (fetchFirefoxAddon {
+          name = "ublock";
+          url = "https://addons.mozilla.org/firefox/downloads/file/4121906/ublock_origin-1.50.0.xpi";
+          sha256 = "10618003e70b528c3f17996e373146d39e6b15f777ac4ca1f214da2ffdb7a5b3";
+        })
+      ];
+    })
   ];
   system.stateVersion = "23.05";
 }
